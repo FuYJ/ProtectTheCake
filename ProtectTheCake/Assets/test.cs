7bd7b3c5f15e87807399ceff66f09852ff9000c9
FuYJ <fuyiru@fuyirudeMacBook-Pro.local>
2016-06-04 15:19:53 +0800

    initialize

---
+++ ProtectTheCake/Assets/test.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum; 
+
+public class Test : MonoBehaviour {
+
+	Image<Bgr, byte> img;
+
+	// Use this for initialization
+	void Start () {
+		img = new Image<Bgr, byte> ("/Users/fuyiru/Pictures/downloaded/green1.jpg");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
